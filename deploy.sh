a92ca35b2938ab79de1153189ef5a319d04182d4
Maksym Sobolyev <sobomax@sippysoft.com>
2019-06-08 20:36:20 -0700

    o Move fuctionality into the deploy script;
    o run hugo and commit changes if updates are made.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 echo -e "\033[0;32mDeploying updates to Github...\033[0m"
+
+OWN_REPO="https://${GITHUB_TOKEN}@github.com/sippy/www.rtpproxy.org.git"
 
 # Build the project.
 hugo -t herring-cove
@@ -8,13 +10,18 @@ hugo -t herring-cove
 # Add changes to git.
 #git add -A
 
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
+NCHG=`git diff public | wc -l`
+if [ ${NCHG} -gt 0 ]
+then
+  # Commit changes.
+  msg="rebuilding site `date`"
+  if [ $# -eq 1 ]
+    then msg="$1"
+  fi
+  git commit -m "$msg" public
+  git remote add origin_rw "${OWN_REPO}"
 
-# Push source and build repos.
-git push origin master
-git subtree push --prefix=public git@github.com:sippy/www.rtpproxy.org.git gh-pages
+  # Push source and build repos.
+  git push origin_rw master
+fi
+git subtree push --prefix=public "${OWN_REPO}" gh-pages
